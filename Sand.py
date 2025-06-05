848579f548c29ca5542766aac97b60cf3ab759da
Alex Fish <iamalexfish@gmail.com>
2025-06-04 22:24:27 -0500

    Added multiple sand colors

--- Sand.py
+++ Sand.py
@@ -20,7 +20,9 @@ for y in range(grid_height):
             sand[y].append(0)
 
 radius = 5
+color_id = 2
 def input():
+    global color_id
     (x_mouse, y_mouse) = pygame.mouse.get_pos()
     x = int(x_mouse/scale)
     y = int(y_mouse/scale)
@@ -34,25 +36,24 @@ def input():
     
     # Create
     if pygame.mouse.get_pressed()[0]:
-        sand[y][x] = 1
+        sand[y][x] = color_id if sand[y][x] == 0 else sand[y][x]
     
     # Create Chunks
-    if pygame.mouse.get_pressed()[1]:
-        sand[y][x] = 1
+    elif pygame.mouse.get_pressed()[1]:
+        sand[y][x] = color_id if sand[y][x] == 0 else sand[y][x]
         # Prevent out of array bounds when - or + index
         # Fill
         if (y > radius and y < grid_height-radius) and (x > radius and x < grid_width-radius):
             for i in range(radius):
                 for j in range(radius):
-                    sand[y+i][x+j] = 1
-                    sand[y+i][x-j] = 1
-                    sand[y-i][x+j] = 1
-                    sand[y-i][x-j] = 1
+                    sand[y+i][x+j] = color_id if sand[y+i][x+j] == 0 else sand[y+i][x+j]
+                    sand[y+i][x-j] = color_id if sand[y+i][x-j] == 0 else sand[y+i][x-j]
+                    sand[y-i][x+j] = color_id if sand[y-i][x+j] == 0 else sand[y-i][x+j]
+                    sand[y-i][x-j] = color_id if sand[y-i][x-j] == 0 else sand[y-i][x-j]
     # Delete
     elif pygame.mouse.get_pressed()[2]:
         sand[y][x] = 0
         # Prevent out of array bounds when - or + index
-        # Fill
         if (y > radius and y < grid_height-radius) and (x > radius and x < grid_width-radius):
             for i in range(radius):
                 for j in range(radius):
@@ -61,32 +62,52 @@ def input():
                     sand[y-i][x+j] = 0
                     sand[y-i][x-j] = 0
 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_1]:
+        color_id = 1
+    elif keys[pygame.K_2]:
+        color_id = 2
+    elif keys[pygame.K_3]:
+        color_id = 3
+
 def update():
     for y in range(grid_height-2, -1, -1):
         for x in range(grid_width):
             # If point has sand but empty below, fall 
-            if sand[y][x] == 1 and sand[y+1][x] == 0:
+            sand_color_id = sand[y][x]
+            if sand[y][x] > 0 and sand[y+1][x] == 0:
                 sand[y][x] = 0
-                sand[y+1][x] = 1
+                sand[y+1][x] = sand_color_id
             #, otherwise we know below us contains sand, 
             #, but move on the x if random value (left or right) below us is empty, move
-            elif sand[y][x] == 1:
+            elif sand[y][x] > 0:
                 # Prevent out of array bounds when - or + index
                 if x > 0 and x < grid_width-2:
                     dir = random.randint(-1, 1)
                     if sand[y+1][x+dir] == 0:
                         sand[y][x] = 0
-                        sand[y+1][x+dir] = 1
+                        sand[y+1][x+dir] = sand_color_id
 
 def draw():
     screen.fill((255,255,255))
     for y in range(grid_height):
         for x in range(grid_width):
-            if sand[y][x] == 1:
-                r = y
-                if y > 240:
-                    r = 240
-                pygame.draw.rect(screen, (240-r, 0, 0), (x*scale, y*scale, scale, scale))#, 1)
+            sand_color_id = sand[y][x]
+            color = None
+            if sand_color_id > 0:
+                # Determine gradient
+                gradient = y
+                if y > 255:
+                    gradient = 255
+                
+                # Determine color
+                if sand_color_id == 1:
+                    color = (255-gradient, 255-gradient, 100)
+                elif sand_color_id == 2:
+                    color = (255-gradient, 0, 0)
+                elif sand_color_id == 3:
+                    color = (0, 255-gradient, 0)
+                pygame.draw.rect(screen, color, (x*scale, y*scale, scale, scale))#, 1)
             # else:
             #     pygame.draw.rect(screen, (128, 128, 128), (x*scale, y*scale, pixel_size*scale, pixel_size*scale), 1)
     pygame.display.flip()
